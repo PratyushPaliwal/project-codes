fb4c4ace4cf97a8d6f1cedf6f0d164725628e0e6
Pratyush Paliwal <paliwalpratyush1510@gmail.com>
2021-10-04 23:01:53 +0530

    add test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("this is test file");
+const var = [5,4,3,2];
+console.log("var array is: " + var);
+
+if(var.length === 4 ){
+  console.length("length of array is" + var.length);
+}
+else
+  console.length("Length is certainly not 4");
+
